6bbddb0849959e1da803d2815ed76743ef3bcca3
hvduong <hd2005555@gmail.com>
2026-04-24 20:55:50 +0700

    Add JwtAuthResponse DTO for JWT authentication response

---
+++ src/main/java/com/hvduong/detectiontomatoes/model/dto/JwtAuthResponse.java
@@ -0,0 +1,17 @@
+package com.hvduong.detectiontomatoes.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class JwtAuthResponse {
+    private String accessToken;
+    private String tokenType = "Bearer";
+
+    public JwtAuthResponse(String accessToken) {
+        this.accessToken = accessToken;
+    }
+}
