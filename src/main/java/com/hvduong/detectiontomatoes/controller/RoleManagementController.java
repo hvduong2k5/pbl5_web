257ddd0b729f1a2dadc1b87ea6a42ddd9600f4f3
hvduong <hd2005555@gmail.com>
2026-04-26 00:15:29 +0700

    Add role management endpoints for retrieving, updating, and deleting roles

--- src/main/java/com/hvduong/detectiontomatoes/controller/RoleManagementController.java
+++ src/main/java/com/hvduong/detectiontomatoes/controller/RoleManagementController.java
@@ -3,6 +3,7 @@ package com.hvduong.detectiontomatoes.controller;
 import com.hvduong.detectiontomatoes.model.dto.PermissionResponseDTO;
 import com.hvduong.detectiontomatoes.model.dto.RoleCreateDTO;
 import com.hvduong.detectiontomatoes.model.dto.RoleResponseDTO;
+import com.hvduong.detectiontomatoes.model.dto.RoleUpdateDTO;
 import com.hvduong.detectiontomatoes.service.RoleService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
@@ -28,11 +29,36 @@ public class RoleManagementController {
         return ResponseEntity.ok(roleService.getAllPermissions());
     }
 
+    @GetMapping("/roles")
+    public ResponseEntity<List<RoleResponseDTO>> getAllRoles() {
+        return ResponseEntity.ok(roleService.getAllRoles());
+    }
+
     @PostMapping("/roles")
     public ResponseEntity<?> createRole(@Valid @RequestBody RoleCreateDTO dto) {
         try {
             RoleResponseDTO savedRole = roleService.createRole(dto);
             return ResponseEntity.status(HttpStatus.CREATED).body(savedRole);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/roles/{id}")
+    public ResponseEntity<?> updateRole(@PathVariable Integer id, @Valid @RequestBody RoleUpdateDTO dto) {
+        try {
+            RoleResponseDTO updatedRole = roleService.updateRole(id, dto);
+            return ResponseEntity.ok(updatedRole);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @DeleteMapping("/roles/{id}")
+    public ResponseEntity<?> deleteRole(@PathVariable Integer id) {
+        try {
+            roleService.deleteRole(id);
+            return ResponseEntity.ok("Xóa Role thành công.");
         } catch (IllegalArgumentException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
         }
