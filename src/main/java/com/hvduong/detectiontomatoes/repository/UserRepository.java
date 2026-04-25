2040d3d70e4143e9486f23a32012feade0becf1a
hvduong <hd2005555@gmail.com>
2026-04-26 00:31:39 +0700

    Add pagination support for searching users by username in UserRepository

--- src/main/java/com/hvduong/detectiontomatoes/repository/UserRepository.java
+++ src/main/java/com/hvduong/detectiontomatoes/repository/UserRepository.java
@@ -1,6 +1,8 @@
 package com.hvduong.detectiontomatoes.repository;
 
 import com.hvduong.detectiontomatoes.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+    
+    Page<User> findByUsernameContainingIgnoreCase(String keyword, Pageable pageable);
 }
