f729c2bd6042fea9fb19695c1fd836f2915757aa
hvduong <hd2005555@gmail.com>
2026-05-08 22:21:57 +0700

    chore: remove ControlCommandDTO class as part of system control decommissioning(#39)

--- src/main/java/com/hvduong/detectiontomatoes/model/dto/ControlCommandDTO.java
+++ src/main/java/com/hvduong/detectiontomatoes/model/dto/ControlCommandDTO.java
@@ -1,15 +1,2 @@
 package com.hvduong.detectiontomatoes.model.dto;
-
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class ControlCommandDTO {
-    private String command; // START / STOP
-}
-
+// DECOMMISSIONED
